de4a69640ba935d2b8119deb20e8b9021636428e
Ipadeola Kabir <ipadeola4kay@gmail.com>
2024-06-28 18:44:57 +0100

    Implement add student feature

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "student.h"
 #include "greetings.c"
 #include "remove_student.c"
+#include "add_student.c"
 
 #define MAX_STUDENTS 100
 
@@ -50,7 +51,13 @@ int main() {
         switch (choice) {
             case 1:
                 // Add Student
-                printf("Functionality in progress!\n");
+                if (num_students >= MAX_STUDENTS) {
+                    printf("Maximum number of students reached. Cannot add more.\n");
+                    printf("-------------------------------------------------\n");
+                    printf("=================================================\n");
+                    break;
+                }
+                add_student(students, &num_students);
                 break;
             case 2:
                 // Modify Student
